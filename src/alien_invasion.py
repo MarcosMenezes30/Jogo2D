c1bdc6d64635012f5a3fc12cf39dc426dfbfdf86
MarcosMenezes30 <quinhotdm@gmail.com>
2026-03-17 14:37:12 -0300

    Solid principles application

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -34,6 +34,8 @@ class AlienInvasion:
             pygame.sprite.Group()
         )  # Cria um grupo para armazenar os alienígenas presentes no jogo
 
+    
+    
     def create_fleet(self):
         """Cria uma frota de alienígenas."""
         # Cria um alienígena e calcula o número de alienígenas em uma linha
@@ -60,6 +62,48 @@ class AlienInvasion:
                 alien.rect.y = alien.y
                 self.aliens.add(alien)
 
+    def check_keycaps(self):
+        """Verifica os eventos de teclado para controlar 
+            a nave e disparar projéteis"""
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif (
+                event.type == pygame.KEYDOWN
+            ):  # Detecta quando uma tecla é pressionada
+                if (
+                    event.key == pygame.K_RIGHT
+                ):  # Verifica se a tecla pressionada é a seta para a direita
+                    self.ship.moving_right = True
+                elif (
+                    event.key == pygame.K_LEFT
+                ):  # Verifica se a tecla pressionada é a seta para a esquerda
+                    self.ship.moving_left = True
+                elif (
+                    event.key == pygame.K_SPACE
+                ):  # Verifica se a tecla pressionada é a barra de espaço
+                    if (
+                        len(self.bullets) < self.settings.bullet_allowed
+                    ):  # Verifica se o número de projéteis na tela excede o limite permitido
+                        new_bullet = Bullet(
+                            self.screen, self.settings, self.ship
+                        )  # Cria um novo projétil
+                        # Aqui seria necessário adicionar o novo projétil a um grupo de projéteis para que ele possa ser atualizado e desenhado na tela
+                        self.bullets.add(
+                            new_bullet
+                        )  # Adiciona o novo projétil ao grupo de projéteis
+
+            elif event.type == pygame.KEYUP:  # Detecta quando uma tecla é liberada
+                if (
+                    event.key == pygame.K_RIGHT
+                ):  # Verifica se a tecla liberada é a seta para a direita
+                    self.ship.moving_right = False
+                elif (
+                    event.key == pygame.K_LEFT
+                ):  # Verifica se a tecla liberada é a seta para a esquerda
+                    self.ship.moving_left = False
+
     def run_game(self):
         """Cria um laço de repetição para a tela sempre ficar visível até
         que o usuário decida fechar a janela."""
@@ -67,43 +111,9 @@ class AlienInvasion:
         self.create_fleet()  # Cria a frota de alienígenas para ser desenhada na tela
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif (
-                    event.type == pygame.KEYDOWN
-                ):  # Detecta quando uma tecla é pressionada
-                    if (
-                        event.key == pygame.K_RIGHT
-                    ):  # Verifica se a tecla pressionada é a seta para a direita
-                        self.ship.moving_right = True
-                    elif (
-                        event.key == pygame.K_LEFT
-                    ):  # Verifica se a tecla pressionada é a seta para a esquerda
-                        self.ship.moving_left = True
-                    elif (
-                        event.key == pygame.K_SPACE
-                    ):  # Verifica se a tecla pressionada é a barra de espaço
-                        if (
-                            len(self.bullets) < self.settings.bullet_allowed
-                        ):  # Verifica se o número de projéteis na tela excede o limite permitido
-                            new_bullet = Bullet(
-                                self.screen, self.settings, self.ship
-                            )  # Cria um novo projétil
-                            # Aqui seria necessário adicionar o novo projétil a um grupo de projéteis para que ele possa ser atualizado e desenhado na tela
-                            self.bullets.add(
-                                new_bullet
-                            )  # Adiciona o novo projétil ao grupo de projéteis
 
-                elif event.type == pygame.KEYUP:  # Detecta quando uma tecla é liberada
-                    if (
-                        event.key == pygame.K_RIGHT
-                    ):  # Verifica se a tecla liberada é a seta para a direita
-                        self.ship.moving_right = False
-                    elif (
-                        event.key == pygame.K_LEFT
-                    ):  # Verifica se a tecla liberada é a seta para a esquerda
-                        self.ship.moving_left = False
+            # Verifica os eventos de teclado para controlar a nave e disparar projéteis
+            self.check_keycaps()
 
             # Redesenha a tela a cada passagem pelo laço
             self.screen.fill(self.bg_color)
